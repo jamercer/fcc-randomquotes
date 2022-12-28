baf079e5027149557dfe3524bd0b9d210daa053f
Jenith Mercer <jamercer2012@gmail.com>
2022-12-28 13:55:01 -0600

    share quote buttons work

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
+let quoteIndex = 0;
 let paperElement;
 let quoteElement;
 let authorElement;
 let twitterBtn;
-let facebookBtn;
+// let facebookBtn;
 let tumblrBtn;
 let animLock = false
 const animLockTime = 600;
@@ -14,7 +15,7 @@ window.onload = () => {
   quoteElement = document.querySelector("#text");
   authorElement = document.querySelector("#author")
   twitterBtn = document.querySelector('#tweet-quote');
-  facebookBtn = document.querySelector('#facebook-quote');
+  // facebookBtn = document.querySelector('#facebook-quote');
   tumblrBtn = document.querySelector('#tumblr-quote');
 
   fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
@@ -34,12 +35,15 @@ animLockTimeout = () => {
   }, animLockTime);
 }
 
+getQuote = () => quotesLibrary.quotes[quoteIndex].quote;
+getAuthor = () => quotesLibrary.quotes[quoteIndex].author
+
 loadNewQuote = () => {
-  let index = Math.floor((quotesLibrary.quotes.length) * Math.random())
-  quoteElement.innerText = quotesLibrary.quotes[index].quote
-  authorElement.innerText = quotesLibrary.quotes[index].author
-  // twitterBtn.attr('href','')
-  // tumblrBtn.attr('href','')
+  quoteIndex = Math.floor((quotesLibrary.quotes.length) * Math.random());
+  quoteElement.innerText = getQuote();
+  authorElement.innerText = getAuthor();
+  twitterBtn.href = `https://twitter.com/intent/tweet?url="${getQuote()}" -${getAuthor()}`;
+  tumblrBtn.href = `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${getAuthor()}&content=${getQuote()}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`;
   paperElement.className = "paper_feeded";
   animLockTimeout()
 }
