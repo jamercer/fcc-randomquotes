6c838a332f9371be9669480eb0b665bef0bc52d8
Jenith Mercer <jamercer2012@gmail.com>
2022-12-28 10:36:40 -0600

    react quotes work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+// import logo from './logo.svg';
+// import './App.css';
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, {}> {
+  render(): React.ReactNode {
+    return (
+      <div id="quote-box">
+        <div id="slot"></div>
+        <div id="paper-container">
+          <div id="paper">
+            <i className="fa fa-quote-left"></i>
+            <div id="text">
+              quote text sdf
+              <br />
+              sd
+              <br />
+              sdf
+            </div>
+            <i className="fa fa-quote-right"></i>
+            <div id="author">author here</div>
+          </div>
+        </div>
+        <div id="slot-shadow"></div>
+        <div id="buttons">
+          <div className="button-well">
+            <a className="btn" id="tweet-quote" href="twitter.com/intent/tweet">
+              {/* <i className="fab fa-twitter-square"></i> */}
+              <FontAwesomeIcon icon={"twitter-square"} />
+            </a>
+            <a className="btn" href="#">
+              <i className="fab fa-facebook-square"></i>
+            </a>
+            <a className="btn" href="#">
+              <i className="fab fa-tumblr-square"></i>
+            </a>
+          </div>
+          <div className="button-well">
+            <div className="btn">
+              <i className="fas fa-eject"></i>
+            </div>
+            <div className="btn" id="new-quote">
+              <i className="fas fa-comment-dots"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/script.js
+++ src/script.js
@@ -1,33 +1,62 @@
-let paper;
+let paperElement;
+let quoteElement;
+let authorElement;
+let twitterBtn;
+let facebookBtn;
+let tumblrBtn;
 let animLock = false
+let quotesLibrary;
 
 window.onload = () => {
-  // store paper
-  paper = document.querySelector("#paper");
+  // store elements
+  paperElement = document.querySelector("#paper");
+  quoteElement = document.querySelector("#text");
+  authorElement = document.querySelector("#author")
+  twitterBtn = document.querySelector('#tweet-quote');
+  facebookBtn = document.querySelector('#facebook-quote');
+  tumblrBtn = document.querySelector('#tumblr-quote');
+
+  fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+    .then((r) => r.json())
+    .then((r) => {
+      quotesLibrary = r;
+    })
+    .then(() => {
+      loadNewQuote();
+    });
 }
 
 animLockTimeout = (time) => {
+  paperElement.className = "paper_feeded";
   animLock = true
   setTimeout(() => {
     animLock = false
   }, time);
 }
 
+loadNewQuote = () => {
+  let index = Math.floor((quotesLibrary.quotes.length) * Math.random())
+  quoteElement.innerText = quotesLibrary.quotes[index].quote
+  authorElement.innerText = quotesLibrary.quotes[index].author
+  twitterBtn.attr('href','')
+  tumblrBtn.attr('href','')
+}
+
 eject = () => {
   if (!animLock) {
-    paper.className = "paper_ejected";
+    paperElement.className = "paper_ejected";
   }
 }
 
 newQuote = () => {
   if (!animLock) {
-    if (paper.className === "paper_ejected") {
-      paper.className = "paper_feeded";
+    if (paperElement.className === "paper_ejected") {
+      loadNewQuote();
       animLockTimeout(1000)
     } else {
-      paper.className = "paper_ejected"
+      paperElement.className = "paper_ejected"
       setTimeout(() => {
-        paper.className = "paper_feeded";
+        loadNewQuote();
       }, 1000);
       animLockTimeout(2000)
     }
